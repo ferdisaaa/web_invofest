aaa1b42775be45e0eba99d6bab75fe609990e823
ferdisaaa <sptraferdi413@gmail.com>
2026-04-29 17:56:46 +0700

    menghapus currentpath

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,7 @@
-import { Home, Trophy, Clipboard, Laptop, Mic, UserCircle } from "lucide-react";
+import { Home, Trophy, Clipboard, Laptop, Mic, UserCircle, } from "lucide-react";
 import { NavLink } from "react-router-dom";
 
 export const Header: React.FC = () => {
-  const currentPath = "#";
 
   const menuItems = [
     { label: "Beranda", href: "/", icon: <Home size={18} /> },
